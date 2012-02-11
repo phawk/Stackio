2797dfb735182781dc4fdee260bab826a18c8a2d
Pete Hawkins <pete@phawk.co.uk>
2012-02-11 23:26:45 +0000

    Initial Commit

---
+++ stackio/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  stackio
+//
+//  Created by Pete Hawkins on 11/02/2012.
+//  Copyright (c) 2012 phawk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ stackio/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  stackio
+//
+//  Created by Pete Hawkins on 11/02/2012.
+//  Copyright (c) 2012 phawk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
