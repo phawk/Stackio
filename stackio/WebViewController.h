2ec152daae72d6817eca85b7eeb9c3fe4db7a7c1
Pete Hawkins <pete@phawk.co.uk>
2012-02-12 15:39:25 +0000

    Add web view placeholder to open actual question on stack overflow mobile site.

---
+++ stackio/WebViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebViewController.h
+//  stackio
+//
+//  Created by Pete Hawkins on 12/02/2012.
+//  Copyright (c) 2012 phawk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebViewController : UIViewController
+
+@end
