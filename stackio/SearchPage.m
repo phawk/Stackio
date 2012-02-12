4bc11e7376713ac2cc4af3d32e3ec20195262a34
Pete Hawkins <pete@phawk.co.uk>
2012-02-12 17:36:20 +0000

    Use own dispatch queue.

--- stackio/SearchPage.m
+++ stackio/SearchPage.m
@@ -11,9 +11,6 @@
 #import "URLEncoder.h"
 #import "WebViewController.h"
 
-#define kBgQueue dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
-#define StackExchangeSearchEndpoint [NSURL URLWithString: @"http://api.stackexchange.com/2.0/search?order=desc&sort=activity&intitle=php&site=stackoverflow"]
-
 @implementation SearchPage
 
 @synthesize searchFormLabel = _searchFormLabel;
@@ -69,11 +66,31 @@
         // Build up our url
         NSURL *endpoint = [NSURL URLWithString: [@"http://api.stackexchange.com/2.0/search?order=desc&sort=activity&site=stackoverflow&intitle=" stringByAppendingString: searchQueryText]];
         
+        // Create a new dispatch queue
+        dispatch_queue_t apiCallQueue = dispatch_queue_create("getQuestions", NULL);
+        
         // Do an async call to get the results
-        dispatch_async(kBgQueue, ^{
+        dispatch_async(apiCallQueue, ^{
             NSData* data = [NSData dataWithContentsOfURL: endpoint];
-            [self performSelectorOnMainThread:@selector(fetchedData:) 
-                                   withObject:data waitUntilDone:YES];
+            
+            // Only if the data returns something usable
+            if (data != nil)
+            {
+                [self performSelectorOnMainThread:@selector(fetchedData:) 
+                                       withObject:data waitUntilDone:YES];
+            }
+            else
+            {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    // We need to popup an alert to tell the user the interwebs are b0rked
+                    UIAlertView *message = [[UIAlertView alloc] initWithTitle:@"Nightmare." 
+                                                                      message:@"There seems to have been a problem with the interwebs" 
+                                                                     delegate:nil
+                                                            cancelButtonTitle:@"Aww, Okay." 
+                                                            otherButtonTitles:nil];
+                    [message show];
+                });
+            }
         });
     }
     else
@@ -98,11 +115,21 @@
     
     self.apiResults = [json valueForKey:@"items"];
     
-    // Log the data that comes back
-    NSLog(@"Total: %d", [self.apiResults count]);
-    
-    // Segue
-    [self performSegueWithIdentifier:@"goResultsView" sender:self];
+    if (self.apiResults.count > 0)
+    {
+        // Segue
+        [self performSegueWithIdentifier:@"goResultsView" sender:self];
+    }
+    else
+    {
+        // No results matched the query
+        UIAlertView *message = [[UIAlertView alloc] initWithTitle:@"No results found" 
+                                                          message:@"Pro Tip: Try making your search less specific" 
+                                                         delegate:nil
+                                                cancelButtonTitle:@"Thanks!"
+                                                otherButtonTitles:nil];
+        [message show];
+    }
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
